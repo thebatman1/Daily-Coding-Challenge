029cd71729d8da62107a621070a0230585be4e24
thebatman1 <mrinmay.mukherjee1@gmail.com>
2018-12-07 02:48:18 +0530

    Serialize and First Missing no done

--- FirstMissingPositiveInteger/first_missing_pos_int.cpp
+++ FirstMissingPositiveInteger/first_missing_pos_int.cpp
@@ -1,25 +1,74 @@
+/**
+ * Input: An array containing positive and negative values
+ * Output: The first missing positive integer
+ */
+
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
+int segregate(int [], int);
+int find_missing_positive(int [], int);
+int find_missing(int [], int);
+
 int main(void) {
 	int n;
 	cout << "Enter no of elements: ";
 	cin >> n;
+	cout << "Enter the elements: ";
 	int a[n];
 	for (int i = 0; i < n; ++i) {
 		cin >> a[i];		
 	}
-	int max_n = INT_MAX, min_n = INT_MIN;
-	for (int i = 0; i < n; ++i) {
-		if (a[i] > 0) {
-			if (a[i] > max_n) {
-				max_n = a[i];	
-			} else if (a[i] < min_n) {
-				min_n = a[i];	
-			}
-		}	
-	}	
+	int missing = find_missing(a, n);
+	cout << "The smallest missing positive number is " << missing << "\n";
 	return 0;
+}
+
+
+/**
+ * Utility function that puts all the non-positive
+ * numbers on the left side of arr[] and return count 
+ * of such numbers
+ */
+int segregate(int a[], int n) {
+	int j = 0, i;
+	for(i = 0; i < n; ++i) {
+		if (a[i] <= 0) {
+			swap(a[i], a[j]);
+			++j;
+		}
+	}
+	return j;
+}
+
+
+/**
+ * Find the smallest positive missing number in an array
+ * that contains all positive integers
+ */
+int find_missing_positive(int a[], int n) {
+	int i;
+	// Mark a[i] as visited by marking a[a[i] - 1] negative.
+	for (i = 0; i < n; ++i) {
+		if (abs(a[i]) - 1 < n && a[abs(a[i]) - 1] > 0) {
+			a[abs(a[i]) - 1] = -a[abs(a[i]) - 1];		
+		}	
+	}
+	for (i = 0; i < n; ++i) {
+		if (a[i] > 0)
+			return i + 1;
+	}
+	return n + 1;
+}
+
+
+/**
+ * Find the smallest positive missing number in an array
+ * that contains both positive and negative integers
+ */
+int find_missing(int a[], int n) {
+	int shift = segregate(a, n);
+	return find_missing_positive(a + shift, n - shift);
 }
--- SerializeDeserializeBinaryTree/serialize_deserialize.py
+++ SerializeDeserializeBinaryTree/serialize_deserialize.py
@@ -1,9 +1,62 @@
 #!/usr/bin/env python3.6
 
 
+MARKER = '-1'
+DELIM = '|'
+
+
 class Node(object):
     """ A class that defines a node in a Binary Tree """
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+def serialize(root, ans=""):
+    """ A function that serializes the Binary Tree to a string """
+    if not root:
+        return ans + MARKER + DELIM
+
+    return (ans + root.val + DELIM + serialize(root.left, ans) +
+            serialize(root.right, ans))
+
+
+def deserialize(serialized):
+    """ A function that deserialize the given string to Binary Tree """
+    nodes = serialized[:-1].split('|')
+    root = None
+    return deserialize_helper(root, nodes)[0]
+
+
+def deserialize_helper(root, nodes, i=0):
+    """ A helper function to deserialize the string to a Binary Tree """
+    node = nodes[i]
+    if node == MARKER:
+        return None, i
+    root = Node(node)
+    root.left, k = deserialize_helper(root.left, nodes, i + 1)
+    root.right, k = deserialize_helper(root.right, nodes, k + 1)
+    return root, k
+
+
+def preorder_traversal(root):
+    """ Function to perform preorder traversal """
+    if not root:
+        return
+    print(root.val + " ")
+    preorder_traversal(root.left)
+    preorder_traversal(root.right)
+
+
+def main():
+    """ The main function """
+    node = Node('root',
+                Node('left',
+                     Node('left.left')),
+                Node('right'))
+    print(deserialize(serialize(node)).left.left.val == 'left.left')
+
+
+if __name__ == '__main__':
+    main()
